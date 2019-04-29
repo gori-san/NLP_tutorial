581d8d2b5518e5da4adc840ffa056a62a05a055b
gori-san <zer0193yamyam@gmail.com>
2019-04-29 11:04:50 +0900

    update script

--- chapter02/train_ngram.py
+++ chapter02/train_ngram.py
@@ -2,7 +2,7 @@ import sys
 from collections import defaultdict
 
 path = sys.argv[1]
-N = sys.argv[2]
+N = int(sys.argv[2])
 counts = defaultdict(lambda: 0)
 context_counts = defaultdict(lambda: 0)
 
@@ -11,19 +11,21 @@ with open(path, 'r', encoding='utf-8') as i_file:
         words = line.strip().split(' ')
         words.insert(0, '<s>')
         words.append('</s>')
-        for i in range(N-1, len(words)-N+1):
-            for j in range(1, N-1):
-                counts[' '.join(words[i-N+j:i])] += 1
-                context_counts[' '.join(words[i-N+j:i-1])] += 1
+        for i in range(N-1, len(words)-N+2):
+            for j in range(1, N):
+                counts[' '.join(words[i-N+j:i+1])] += 1
+                context_counts[' '.join(words[i-N+j:i])] += 1
             counts[words[i]] += 1
             context_counts[''] += 1
 
+print(counts)
+
 context = []
 
-with open('model', 'w', encoding='utf-8') as m_file:
-    for key, value in counts.items():
+with open('model_ngram', 'w', encoding='utf-8') as m_file:
+    for key, value in sorted(counts.items()):
         words = key.split(' ')
         words.pop(len(words)-1)
         context = ' '.join(words)
         probability = counts[key] / context_counts[context]
-        m_file.write(f'{key} {probability}\n')
+        m_file.write(f'{key}\t{probability}\n')
